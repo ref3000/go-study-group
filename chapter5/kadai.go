178402f7b15c0084699f1624b2e9bdfe13990463
yamamoto_kiyohiro <yamamoto_kiyohiro@applibot.co.jp>
2020-03-07 17:58:30 +0900

    chapter5

--- chapter5/kadai.go
+++ chapter5/kadai.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
@@ -12,19 +13,37 @@ import (
 var delimiter = flag.String("d", ",", "区切り文字を指定してください")
 var fields = flag.Int("f", 1, "フィールドの何番目を取り出すか指定してください")
 
-// go-cutコマンドを実装しよう
+func Validate(args []string, fields int) error {
+	if len(args) == 0 {
+		return fmt.Errorf("ファイルパスを指定してください。")
+	}
+	if fields <= 0 {
+		return fmt.Errorf("-f は1以上である必要があります")
+	}
+	return nil
+}
+
+func Cut(src io.Reader, dst io.Writer, delimiter string, fields int) error {
+	scanner := bufio.NewScanner(src)
+	for scanner.Scan() {
+		text := scanner.Text()
+		sb := strings.Split(text, delimiter)
+		if len(sb) < fields {
+			return fmt.Errorf("-fの値に該当するデータがありません")
+		}
+		s := sb[fields-1]
+		fmt.Fprintln(dst, s)
+	}
+	return scanner.Err()
+}
+
 func cut() {
 	flag.Parse()
 
 	// このValidationを関数1つ目に切り出す ---------
 	// ヒント：flagの内容を渡してやって、バリデーションし、エラーがあれば返すような関数にできる
-	if flag.NArg() == 0 {
-		fmt.Fprintln(os.Stderr, "ファイルパスを指定してください。")
-		os.Exit(1)
-	}
-	if *fields < 0 {
-		fmt.Fprintln(os.Stderr, "-f は1以上である必要があります")
-		os.Exit(1)
+	if err := Validate(flag.Args(), *fields); err != nil {
+		log.Fatal(err)
 	}
 	// ---------------------------------------
 
@@ -39,18 +58,7 @@ func cut() {
 	// また、出力も現在fmt.Println(s)にしているが、io.Writerを使って書き出す先を指定できるようにしてやる
 	// 関数の引数で読み出すio.Readerと、
 	// 書き出すio.Writer (本関数からはos.Stdout, テストからはbyte.Bufferなどへ)を指定できるようにすると良い
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		text := scanner.Text()
-		sb := strings.Split(text, *delimiter)
-		if len(sb) < *fields-1 {
-			fmt.Fprintln(os.Stderr, "-fの値に該当するデータがありません")
-			os.Exit(1)
-		}
-		s := sb[*fields-1]
-		fmt.Println(s)
-	}
-	if err = scanner.Err(); err != nil {
+	if err := Cut(file, os.Stdout, *delimiter, *fields); err != nil {
 		log.Fatal(err)
 	}
 	// ------------------------------------------------
